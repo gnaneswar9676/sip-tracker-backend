e5c294c37f0055782b7b026cdced8e75a11d0703
Kollimarla Gnaneswar <2200090109@kluniversity.in>
2026-05-18 15:19:54 +0530

    Added OpenTelemetry logging and monitoring

--- server.js
+++ server.js
@@ -10,22 +10,12 @@ const {
   connectDB
 } = require("./database/pgManager");
 
-
-// CONNECT DATABASE
 connectDB();
 
-
-// CONNECT REDIS
 connectRedis();
 
+const PORT = process.env.PORT || 5000;
 
-const PORT =
-process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-
-  console.log(
-    `Server running on port ${PORT}`
-  );
-
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
